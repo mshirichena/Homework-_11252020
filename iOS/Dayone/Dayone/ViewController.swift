f929bab5a233445d5bc8844436447ccc2801ef26
Field Employee <fieldemployee@Fields-MacBook-Pro.local>
2020-11-25 12:57:36 -0500

    Rich Text

--- iOS/Dayone/Dayone/ViewController.swift
+++ iOS/Dayone/Dayone/ViewController.swift
@@ -20,4 +20,12 @@ class ViewController: UIViewController {
 
 }
 
-
+//Human Interface Guidelines
+//
+//iOS
+//
+//To deliver a product that rises to the top of App Store charts, i will need to meet high expectation for functionality and quality. I will need to focus on three primary themes that Apple offers and they are clarity, deference and depth.To do this effectively, i have to ensure that my texts and Icons are legible, precise & lucid, I have to ensure that the interface is crisp & fluid, enhancing interactions with content and finally I have to make sure that my products have substantive visual, motion 7 functionality that is delightful and gives understanding.There are five principles I will have to keep in mind as i design my applications and these are aesthetic integrity, consistency, direct manipulation , feedback, metaphors and user control.These determine how well my applications appear & behave; the consistency of standards & paradigms of icons, standards text styles & uniformity of terminology; user experience as they manipulate the app, the built-in perceptive feedback from user actions, how well we create virtual object & action metaphors that encourage familiar experiences, and finally how well we enable users while minimizing unwanted outcomes.
+//
+//App Store Review Guidelines
+//
+//The Apple app store review guidelines center on five things that they look at care and these are Safety, Performance, Business, Design, and Legal. The app should not damage user devices, should not be offensive, and should not cause physical harm to users. The app's performance should be tested on-device for bugs, stability, hardware compatibility, software requirements and include a demo account, a process ensuring the final version of the app meets expectations for performance. The business model should be easily verifiable, reflecting how we monetize our apps, ensuring that pricing, in-app purchases, subscriptions, and other business process are acceptable. Our apps have to be in compliance with all legal requirements, including all locations we make it available, especially data privacy regulations, intellectual property rights and other important legal issues that are of utmost importance.
