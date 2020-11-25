7057197aa6d5dbfae71b10123b0596c2abc144fe
Field Employee <fieldemployee@Fields-MacBook-Pro.local>
2020-11-25 12:24:32 -0500

    Completed homework and saved it in the Git Hub sub-folder under the dayone folder

---
+++ Shared/DayoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayoneApp.swift
+//  Shared
+//
+//  Created by Field Employee on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct DayoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/Dayone/Dayone/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Dayone
+//
+//  Created by Field Employee on 11/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        myLabel.text = "Some other text"
+    
+    }
+
+
+}
+
+
